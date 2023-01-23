738e9081810e6073c0d7ca8b3de7942ec6b85dfc
Vitor Feijo <vitorfleonardo@gmail.com>
2023-01-23 11:35:49 -0300

    class pedido

--- src/Model/Funcionario.java
+++ src/Model/Funcionario.java
@@ -1,4 +1,31 @@
 package Model;
 
-public class Funcionario {
-}
+public class Funcionario extends Usuario {
+    private String nome;
+    private String cargo;
+
+    public Funcionario(String email, String senha, String telefone) {
+        super(email, senha, telefone);
+    }
+
+    public Funcionario(String email, String senha, String telefone, String nome, String cargo) {
+        super(email, senha, telefone);
+        this.nome = nome;
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
